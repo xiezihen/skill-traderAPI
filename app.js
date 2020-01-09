f41f18f305b2744dc659bbeae400b646801d010e
xiezihen <frank.xie@mail.utoronto.ca>
2020-01-09 00:14:56 -0500

    mongo url update

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.get('/', (req, res) => {
 
 //db connection
 mongoose.connect(
-    process.env.DB_CONNECTION,
+    process.env.MONGODB_URI,
     { useNewUrlParser: true }, 
      ()=>{
     console.log("connected")
