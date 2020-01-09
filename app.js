f0d72d3257ffa8a58c0be9642aad7b3772d6fb7a
xiezihen <frank.xie@mail.utoronto.ca>
2020-01-09 15:23:20 -0500

    added cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 
 const express = require('express');
 const app = express();
+const cors = require('cors');
 const postsRoute = require('./routes/posts');
 const loginRoute = require('./routes/login');
 const registerRoute = require('./routes/register');
@@ -21,6 +22,7 @@ initializePassport(passport);
 app.use(passport.initialize());
 app.use(passport.session());
 require('./passport-config')(passport);
+app.use(cors());
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
